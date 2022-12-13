7c26922bbb9629be4e991d7b509711cf1e392269
kanchan <kanchan.shastrakar7@gmail.com>
2022-12-14 00:35:31 +0530

    Added space in BaseNew class

--- src/test/java/neoStoxBase/BaseNew.java
+++ src/test/java/neoStoxBase/BaseNew.java
@@ -18,6 +18,8 @@ public class BaseNew
 	    driver.manage().window().maximize();
 		driver.get(UtilityNew.readDataFromPropertyFile("url"));
 		Reporter.log("launching Browser", true);
+		
+		
 		Thread.sleep(1000);
 	}
 
